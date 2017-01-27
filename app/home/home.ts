7ddfaec0a63dd3bf2a192636db616ff38b62f8c3
Cédric Méléard <cedric.meleard@gmail.com>
2017-01-27 08:59:53 +0100

    second commit

--- app/home/home.ts
+++ app/home/home.ts
@@ -8,10 +8,12 @@ import { component } from "../utils/decorators";
 export class Home {
     name : KnockoutObservable<string>;
     users : KnockoutObservableArray<any>;
+    opened : KnockoutObservable<boolean>;
 
     constructor(){
         this.name = ko.observable("a dude");
         this.users = ko.observableArray();
+        this.opened = ko.observable(false);
     }
 
     addUser() : void {
@@ -21,6 +23,12 @@ export class Home {
             //reset name
             this.name('');
         }
+    }
+
+    openMenu() : void {
+        //toggle
+        console.log(`menu is ${this.opened() ? 'open' : 'closed'}`);
+        this.opened(!this.opened());
     }
 
 }
@@ -37,6 +45,29 @@ class UserComponent {
 
     constructor(private params: any){
         this.name = params.name;
+    }
+}
+
+@component( {
+    selector : 'side-nav',
+    template : `
+    <div class="side-menu" data-bind="css : openedClass    ">
+        <div class="icon-menu" data-bind="click : closeMenu">close</div>
+    </div>`
+})
+class SideMenu {
+    opened : KnockoutObservable<boolean>;
+    openedClass : KnockoutComputed<string>;
+
+    constructor(private params : any){
+        //j'ai un doute ici si params n'est pas directement injecté
+        this.opened = params.opened;
+        this.openedClass = ko.computed(() => params.opened() ? 'opened' : '' );
+    }
+
+    closeMenu() : void {
+        console.log(`Closing menu`);
+        this.opened(false);
     }
 }
 
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var ts = require('gulp-typescript');
+var tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('scripts', function() {
+    var tsResult = gulp.src("app/**/*.ts")
+        .pipe(sourcemaps.init())
+        .pipe(tsProject());
+ 
+    return tsResult.js.
+        pipe(sourcemaps.write()).
+        pipe(gulp.dest('built'));
+});
+gulp.task('scripts:watch', () => {
+  gulp.watch('./app/**/*.ts', ['scripts']);
+});
+
+gulp.task('sass', () => {
+  return gulp.src('./sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./styles'));
+});
+ 
+gulp.task('sass:watch', () => {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['scripts','sass'], () => {
+    console.log('code compiled');
+});
+gulp.task('with-watch', ['scripts:watch','sass:watch'], () => {
+    console.log('started watching');
+});
